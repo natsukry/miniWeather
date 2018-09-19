61cd3fb1023a505a78a5aebb573047e194b8e45d
Jason <rishins@qq.com>
2018-09-19 20:55:02 +0800

    init project

---
+++ app/src/main/java/com/aiutopia/jason/miniwearher/MainActivity.java
@@ -0,0 +1,16 @@
+package com.aiutopia.jason.miniwearher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jason on 19/09/2018.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
