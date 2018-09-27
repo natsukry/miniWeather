6c966ba9dc846aee2f1f0644eb54631e78eb3ca7
Jason <rishins@qq.com>
2018-09-27 19:31:29 +0800

    Add NetUtil

--- app/src/main/java/com/aiutopia/jason/miniwearher/MainActivity.java
+++ app/src/main/java/com/aiutopia/jason/miniwearher/MainActivity.java
@@ -2,15 +2,27 @@ package com.aiutopia.jason.miniwearher;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import util.NetUtil;
 
 /**
  * Created by Jason on 19/09/2018.
  */
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORK_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this, "网络OK！", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
